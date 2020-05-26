9afe396d2c45a95a3aee0335d42aa5d046c129fe
Antoine Perry <antoine.ap.57@gmail.com>
2020-05-26 03:33:54 +0200

    removed sniffer script

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ import argparse
 
 from src.mitm.anti_spoofing import run_antispoof
 from src.mitm.mitm import mitm
-from src.mitm.sniffer import sniffer
 from src.mitm.sniffer_http import run as sniff_http
 from src.mitm.sniffer_telnet import run as sniff_telnet
 
 parser = argparse.ArgumentParser()
 parser.add_argument("action", help="what do you want me to do ?",
-                    choices=("sniffer", "telnet", "http", "ARP", "defense"))
+                    choices=("telnet", "http", "ARP", "defense"))
 args = parser.parse_args()
 
-if args.action == "sniffer":
-    sniffer()
-
-elif args.action == "telnet":
+if args.action == "telnet":
     sniff_telnet()
 
 elif args.action == "http":
